36544980f6b1120015953e6b2eba1ad9bee4123f
RuiMSMelo <ruimsmelo@gmail.com>
2025-06-30 13:35:28 +0100

    setup to deploy go ghpages

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,21 @@
+import { defineConfig } from 'vite'
 import restart from 'vite-plugin-restart'
 import glsl from 'vite-plugin-glsl'
 
-export default {
+export default defineConfig({
     root: 'src/',
     publicDir: '../static/',
-    base: './',
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    base: 'https://github.com/RuiMSMelo/3jsjourney_4_7',
+    server: {
+        host: true,
+        open: !(
+            'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+        ),
     },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: true,
     },
-    plugins:
-    [
-        restart({ restart: [ '../static/**', ] }), // Restart server on static file change
-        glsl() // Handle shader files
-    ]
-}
+    plugins: [restart({ restart: ['../static/**'] }), glsl()],
+})
